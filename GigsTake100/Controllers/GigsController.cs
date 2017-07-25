0ab1fd62c7109e2456b755f6c28f76322e7a9622
idomijan <idomijan1@gmail.com>
2017-07-25 16:15:52 +0200

    Building a view, form for gig

---
+++ GigsTake100/Controllers/GigsController.cs
@@ -0,0 +1,28 @@
+﻿using GigsTake100.Models;
+using GigsTake100.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigsTake100.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigsTake100/Viewmodels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GigsTake100.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigsTake100.Viewmodels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}

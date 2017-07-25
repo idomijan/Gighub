2e76a831fe687e7c9c603210b8566e7893d8e13c
idomijan <idomijan1@gmail.com>
2017-07-25 14:41:15 +0200

    Gighub fisrt migrations and domain classes

---
+++ GigsTake100/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigsTake100.Startup))]
+namespace GigsTake100
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
